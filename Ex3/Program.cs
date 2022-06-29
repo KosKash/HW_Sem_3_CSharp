405ebbde40cb05200cc5e5c37f85eee3680e55d9
Constantine <koskash98ksband@gmail.com>
2022-06-30 00:03:59 +0600

    Hw3Done

---
+++ Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
+Console.Clear();
+Console.Write("Введите число ");
+double n = double.Parse(Console.ReadLine());
+int ind = 0;
+double num = 0;
+while(n > ind - 1)
+{
+    num = Math.Pow(ind,3);
+    Console.Write(num + " ");
+    ind++;
+}
