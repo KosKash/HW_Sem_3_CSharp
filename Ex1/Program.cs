0b096c8c2e52640a67b6ac24b141b4bae99c7dcb
Constantine <koskash98ksband@gmail.com>
2022-06-29 22:14:53 +0600

    Optimized

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -7,51 +7,23 @@ char[] msn = num.ToCharArray();
 int i = msn.Length - 1;
 int j = 0;
 int half = msn.Length / 2;
-int ostdel = msn.Length % 2;
 int count = 1;
-
-
-if (ostdel == 0)
+while (half != count)
 {
-    while (half != count)
+    if (msn[j] == msn[i])
     {
-        if (msn[j] == msn[i])
-        {
-            j = j + 1;
-            i = i - 1;
-            count++;
-        }
-        if (msn[j] != msn[i])
-        {
-            System.Console.WriteLine("Число " + num + " не является палиндромом");
-            break; //Не знал как решить, при условии что "число не полиндром" в консоли спамило это сообщение, скоре всего это связано с циклом "while"
-        }
+        j = j + 1;
+        i = i - 1;
+        count++;
+    }
+    if (msn[j] != msn[i])
+    {
+        System.Console.WriteLine("Число " + num + " не является палиндромом");
+        break; //Не знал как решить, при условии что "число не полиндром" в консоли спамило это сообщение, скоре всего это связано с циклом "while"
+    }
 
-    }
-    if (count == half)
-    {
-        Console.WriteLine("Число " + num + " это палиндром");
-    }
 }
-if (ostdel == 1)
+if (count == half)
 {
-    while (half != count)
-    {
-        if (msn[j] == msn[i])
-        {
-            j = j + 1;
-            i = i - 1;
-            count++;
-        }
-        if (msn[j] != msn[i])
-        {
-            System.Console.WriteLine("Число " + num + " не является палиндромом");
-            break; //Не знал как решить, при условии что число не полиндром в консоли спамило это сообщение, скоре всего это связано с циклом "while"
-        }
-
-    }
-    if (count == half)
-    {
-        Console.WriteLine("Число " + num + " это палиндром");
-    }
+    Console.WriteLine("Число " + num + " это палиндром");
 }
